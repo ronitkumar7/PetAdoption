f2173bc203c2765843ecf77202fbed549cf4ee93
m-grigoriu <marcgrigoriu2003@gmail.com>
2023-11-15 15:36:42 -0500

    Application works, added pagination.

--- P2/petpal/petpal/settings.py
+++ P2/petpal/petpal/settings.py
@@ -134,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # rest framework
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
