15eae33666262875b88f3fe144d52723cb56858e
ronitkumar7 <rk4life2018@gmail.com>
2023-11-15 16:45:53 -0500

    shell files

--- P2/run.sh
+++ P2/run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python3 /petpal/manage.py runserver
--- P2/startup.sh
+++ P2/startup.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 apt install python3-pip
 apt install python3-virtualenv
-virtualenv /venv
+virtualenv venv
 source venv/bin/activate
 pip install -r packages.txt
 python3 /petpal/manage.py makemigrations
