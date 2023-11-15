b94c332f6c0776bfe009a40aedc7220cfc1e84b8
ronitkumar7 <rk4life2018@gmail.com>
2023-11-14 23:26:41 -0500

    fix?

---
+++ P2/petpal/account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import
+
+app_name="accounts"
+urlpatterns = [
+    # path('users/', , name='users'),
+    # path('shelters/', , name='shelters'),
+    # path('user/<int:pk>/', , name='user_id'),
+    # path('shelter/<int:pk>/', , name='shelter_id'),
+
+
+]
