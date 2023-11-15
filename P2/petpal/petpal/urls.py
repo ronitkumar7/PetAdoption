48b09643958250f312bb8272e78dd3e2b58d2c0e
maxXu0713 <max.xu.0713@gmail.com>
2023-11-14 23:11:19 -0500

    Revert "first"
    This reverts commit 9da61e00df05666897ae31e2af3b9592e3d97441.

--- P2/petpal/petpal/urls.py
+++ P2/petpal/petpal/urls.py
@@ -1,5 +1,6 @@
 """
 URL configuration for petpal project.
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
@@ -17,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('accounts/', include('account.urls', namespace='accounts')),
     path('comments/', include('comment.urls', namespace='comments')),
